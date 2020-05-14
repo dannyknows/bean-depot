661ecf546608c2ca2c0354bfef6dbf2fbada7528
dannyknows <dannylow465@gmail.com>
2020-05-14 17:55:43 +1000

    updated controller logic and routes for new pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def index
     @products = Product.all
+    p params
+  end
+  def cart
+    @current_user = User.find_by(id: 7)
+    cart = Cart.find_by(user_id: @current_user.id)
+    @current_cart = cart.cart_products
+  end
+  def showcase
+    @product = Product.find_by(id: params[:id])
   end
 end
