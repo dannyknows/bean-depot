82a095beb08e8ac412baed16fbf148c5127a3316
Danny_knows <dannylow456@gmail.com>
2020-05-18 22:43:24 +1000

    created cart helpers for displaying currency price and cart total

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,13 @@
 module CartsHelper
+  def display_price(int)
+    ActionController::Base.helpers.number_to_currency(int)
+  end
+
+  def total(cart)
+    total = 0
+    cart.each do |cart|
+      total += cart.product.price
+    end
+    display_price(total)
+  end
 end
