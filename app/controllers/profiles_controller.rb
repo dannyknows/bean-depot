91726bb9e7e8298e87f60b5a3e4ffab49b0da7e6
Danny_knows <dannylow456@gmail.com>
2020-05-18 22:35:26 +1000

    created profiles controller, with crud funtionality

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,32 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
   
-  def profile
-
-  end
 
   def company_details
-    
   end
 
+  def edit
+   
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to profiles_path
+  end
+
+  def admin
+    @users = User.all
+    @producers = Producer.all
+  end
+
+  private
   
+  def user_params
+    user_params = params.require(:user).permit(:fname, :lname, :email)
+  end
+
+  def find_user
+    @user = User.find_by(id: current_user.id)
+  end
 end
