3c29174d1cf719f00204cee8e05c2f3851e52a04
Danny_knows <dannylow456@gmail.com>
2020-05-18 22:40:32 +1000

    added S3 functionality. has_one_attatched

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :producer
   belongs_to :origin
+  has_one_attached :image
   enum grind_type: { AEROPRESS: 0, COLD_BREW: 1, ESPRESSO: 2, PLUNGER: 3 }
-  def display_price
-    ActionController::Base.helpers.number_to_currency(self.price)
-  end
 end
